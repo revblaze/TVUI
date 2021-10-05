e0c09f0284792525841ed5ad830c5ae685b031ab
Justin Bush <revblaze@users.noreply.github.com>
2021-10-05 10:32:42 -0600

    Create base iOS project

---
+++ iOS/TVUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TVUI
+//
+//  Created by Justin Bush on 2021-10-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS/TVUI/TVUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TVUIApp.swift
+//  TVUI
+//
+//  Created by Justin Bush on 2021-10-05.
+//
+
+import SwiftUI
+
+@main
+struct TVUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
